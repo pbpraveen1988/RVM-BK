229cb859c981e4d50972e2f302e32374a7164c3d
PraveenKumar <pb.praveen@xymob.in>
2021-08-28 01:02:11 +0530

    Will add the POST  and patch request.

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,12 +1,23 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export class Connections {
-    public static postgres : TypeOrmModuleOptions = {
-        type: 'postgres',
-        port: 5432,
-        username: 'postgres',
+    // public static postgres: TypeOrmModuleOptions = {
+    //     type: 'mysql',
+    //     port: 3306,
+    //     username: 'admin',
+    //     password: 'danialdanish',
+    //     database: 'rvm-danial',
+    //     host: 'rvm-danial.c7rknp0qzqv8.us-east-2.rds.amazonaws.com',
+    //     synchronize: true,
+    //     entities: []
+    // };
+    public static postgres: TypeOrmModuleOptions = {
+        type: 'mysql',
+        port: 3306,
+        username: 'root',
         password: '123456',
-        database: 'typescriptLocal',
+        database: 'rvm-danial',
+        host: 'localhost',
         synchronize: true,
         entities: []
     };
--- src/utils/QueryBuilder.ts
+++ src/utils/QueryBuilder.ts
@@ -1,8 +1,37 @@
 import { Utils } from './Utils';
-import { QueryParams } from '../model';
+import { QueryParams, Record } from '../model';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 
 export class QueryBuilder {
+
+    public static async updateRecord(object: string, recordId: string, request: Record) {
+        if (object && recordId && request) {
+            let columns = ' ';
+            Object.keys(request).forEach(x => {
+                columns += `${x} = '${request[x]}',`;
+            })
+            const queryString = `UPDATE ${object} SET ${columns.slice(0, -1)} WHERE id = ${recordId}`;
+            return await Utils.executeQuery(queryString);
+        } else {
+            throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public static async saveRecord(object: string, request: Record) {
+        if (request && object) {
+            let columns = ' ', values = ' ';
+            Object.keys(request).forEach(x => {
+                columns += `${x},`
+                values += `'${request[x]}',`;
+            })
+
+            const queryString = `INSERT INTO ${object} (${columns.slice(0, -1)}) VALUES (${values.slice(0, -1)})`;
+            return await Utils.executeQuery(queryString);
+        } else {
+            throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+        }
+    }
 
     public static createWhereStatement = (whereClause: string): string => {
         const _whereClauses = whereClause.split(',');
@@ -33,12 +62,13 @@ export class QueryBuilder {
         if (queryParams && queryParams.select) {
             const _fields = queryParams.select === 'all' ? '*' : queryParams.select;
             queryString = `Select ${_fields} from ${tableName} `;
+        } else {
+            queryString = `Select id,name from ${tableName} `;
         }
         if (queryParams && queryParams.where) {
             queryString += QueryBuilder.createWhereStatement(queryParams.where);
         }
+        console.log(queryString);
         return await Utils.executeQuery(queryString);
     }
-
-
 }
