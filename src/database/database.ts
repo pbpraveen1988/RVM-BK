f5030348e4815c0747552e924ebfd900ab014041
PraveenKumar <pb.praveen@xymob.in>
2021-08-28 17:14:45 +0530

    update the db file.

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,24 +1,24 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export class Connections {
-    // public static postgres: TypeOrmModuleOptions = {
-    //     type: 'mysql',
-    //     port: 3306,
-    //     username: 'admin',
-    //     password: 'danialdanish',
-    //     database: 'rvm-danial',
-    //     host: 'rvm-danial.c7rknp0qzqv8.us-east-2.rds.amazonaws.com',
-    //     synchronize: true,
-    //     entities: []
-    // };
     public static postgres: TypeOrmModuleOptions = {
         type: 'mysql',
         port: 3306,
-        username: 'root',
-        password: '123456',
+        username: 'admin',
+        password: 'danialdanish',
         database: 'rvm-danial',
-        host: 'localhost',
+        host: 'rvm-danial.c7rknp0qzqv8.us-east-2.rds.amazonaws.com',
         synchronize: true,
         entities: []
     };
+    // public static postgres: TypeOrmModuleOptions = {
+    //     type: 'mysql',
+    //     port: 3306,
+    //     username: 'root',
+    //     password: '123456',
+    //     database: 'rvm-danial',
+    //     host: 'localhost',
+    //     synchronize: true,
+    //     entities: []
+    // };
 }
