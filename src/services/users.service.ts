54afde18123886e95044a0bb9e8e8ef533788ef6
PraveenKumar <pb.praveen@xymob.in>
2021-09-03 22:45:12 +0530

    Will throw the exception on signin

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { QueryBuilder, Utils } from 'src/utils';
 import { Record } from '../model';
 
@@ -12,7 +12,10 @@ export class UserService {
 
   async signIn(request: Record): Promise<any> {
     let queryString = `SELECT * from users where email = '${request.email}' AND password = '${request.password}'`;
-    const data = await Utils.executeQuery(queryString);
-    return data;
+    const data: any = await Utils.executeQuery(queryString);
+    if (data && data.length > 0) {
+      return data[0];
+    }
+    throw new HttpException('Invalid Username or Password', HttpStatus.UNAUTHORIZED);
   }
 }
