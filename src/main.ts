04e40a6efdd36f433e7c8aff47d7df7d083a0167
Anil Kumar Pandey <anil.pandey@innodeed.com>
2021-10-12 09:58:01 +0530

    Add docker

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(80);
 }
 bootstrap();
