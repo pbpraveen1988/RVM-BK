a3ef0f30c425786125339fb251e243cb63f6a57b
PraveenKumar <pb.praveen@xymob.in>
2021-10-29 08:47:35 +0530

    Will add the timeout to 10 minutes

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { AppModule } from './module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as bodyParser from 'body-parser';
+var timeout = require('connect-timeout')
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.useStaticAssets(join('dist','public'));
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.use(bodyParser.json({ limit: '500mb' }));
+  app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+  app.use(timeout('10m'))
   await app.listen(80);
 }
 bootstrap();
